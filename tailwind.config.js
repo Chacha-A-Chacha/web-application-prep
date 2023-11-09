f4e8814a5bbf086fdd0ef354471482d11a342b10
Chacha-A-Chacha <calex2607@gmail.com>
2023-11-09 05:46:39 +0300

    new file:   src/index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./src/**/*.{html,js}",
+    ".{html}"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // ...
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
